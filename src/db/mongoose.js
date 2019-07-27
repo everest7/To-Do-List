b053aa86b887064d81036d8b75d082b85bd25a6a
Bob <everest115@outlook.com>
2019-07-27 10:18:09 -0700

    changing env variables

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONDB_URL, {
+mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false
