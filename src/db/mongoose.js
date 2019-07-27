3f71984aa361764de977d0ebbe68609381e2e699
Bob <everest115@outlook.com>
2019-07-27 10:22:50 -0700

    setting MondoDB URL

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 
+// MONGODB_URL is set on another file to security
 mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useCreateIndex: true,
